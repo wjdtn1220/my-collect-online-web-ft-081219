9d19f8b60236b23bf30993748bcb24d2d4f1330c
Jeong Park <wjdtn1220@gmail.com>
2019-08-17 22:03:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   new_collection = []
   while i < array.length
-    new_collection.push yield(array[i])
+    new_collection << yield(array[i])
     i += 1
   end
   new_collection
